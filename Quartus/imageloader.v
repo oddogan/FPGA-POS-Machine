e6639949911bfb9c961dac2c83918538baee737d
TheMenes <62949868+TheMenes@users.noreply.github.com>
2021-06-21 00:37:24 +0300

    deneme

--- Quartus/imageloader.v
+++ Quartus/imageloader.v
@@ -5,6 +5,7 @@ module imageloader(clk, x, y, red, green, blue);
     localparam image_width = 48;
     localparam image_height = 48;
 	 integer index = 0;
+	 integer file;
 
     // Coordinates in the display
     input [9:0] x, y;
@@ -12,26 +13,63 @@ module imageloader(clk, x, y, red, green, blue);
     // The RGB values in the corresponding pixel
     reg [7:0] r, g, b;
 	 output [7:0] red, green, blue;
-    reg [7:0] data [0:3*(image_width*image_height)-1];
-
+    reg [7:0] apple [0:3*(image_width*image_height)-1];
+	 //reg [7:0] banana [0:3*(image_width*image_height)-1];
+	 //reg [7:0] rgb [0:2];
+	 
     initial
-        $readmemh("bitmap.txt", data);
+	 begin
+			//file = $fopenr("facebook.txt");
+        //$readmemh("apple.txt", apple);
+		  //$readmemh("banana.txt", banana);
+	 end
 
+
+	 
     always @(posedge clk)
     begin
-			index = (y * image_width + x) * 3;
+			//index = ((y-100) * image_width + (x-200)) * 3;
+			
         if (y < image_height && x < image_width)
         begin
-            r <= data[index];
-            g <= data[index+1];
-            b <= data[index+2]; 
+            //$fread(file, "%h %h %h", index, rgb); 
+				r <= 'd45;
+				g <= 'd120;
+				b <= 'd135;
         end
+		  
+		  /*
+		  else if (y >= 100 && y<image_height+100 && x>199 && x<image_width+200  )
+		  begin 
+				index = ((y-100) * image_width + (x-200)) * 3;
+				/*if(apple[index]=='hff &&apple[index+1]=='hff && apple[index+2]== 'hff)
+					begin
+					r <= 'd45;
+					g <= 'd120;
+					b <= 'd135;
+					end
+				else
+					//begin
+					r <= apple[index];
+					g <= apple[index+1];
+					b <= apple[index+2];
+					//end
+				
+				
+		  end*/
+		  
         else
         begin
-            r <= 8'b0;
-            g <= 8'b0;
-            b <= 8'b0;
+            r <= 'd45;
+            g <= 'd120;
+            b <= 'd135;
         end
+		  if(x==0 && y==0)
+				begin
+				r<=150;
+				g<=150;
+				b<=150;
+				end
     end
 
 	assign red = r;
--- Quartus/output_files/FPGAPOSMachine.cdf
+++ Quartus/output_files/FPGAPOSMachine.cdf
@@ -3,10 +3,6 @@ JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
-	P ActionCode(Ign)
-		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
-	P ActionCode(Cfg)
-		Device PartName(5CSEMA5F31) Path("C:/Users/menes/Documents/GitHub/FPGA-POS-Machine/Quartus/output_files/") File("FPGAPOSMachine.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
