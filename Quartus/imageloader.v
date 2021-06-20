580210096286700d27aefda97efb997ba8ffcc6b
bkoyun <86099722+bkoyun@users.noreply.github.com>
2021-06-21 01:23:36 +0300

    fruits

--- Quartus/imageloader.v
+++ Quartus/imageloader.v
@@ -2,8 +2,8 @@ module imageloader(clk, x, y, red, green, blue);
 
 	input clk;
 	
-    localparam image_width = 48;
-    localparam image_height = 48;
+    localparam image_width = 70;
+    localparam image_height = 70;
 	 integer index = 0;
 	 integer file;
 
@@ -14,21 +14,21 @@ module imageloader(clk, x, y, red, green, blue);
     reg [7:0] r, g, b;
 	 output [7:0] red, green, blue;
     reg [7:0] apple [0:3*(image_width*image_height)-1];
-	 //reg [7:0] banana [0:3*(image_width*image_height)-1];
-	 //reg [7:0] rgb [0:2];
+	 reg [7:0] banana [0:3*(image_width*image_height)-1];
+	 reg [7:0] rgb [0:2];
 	 
     initial
 	 begin
 			//file = $fopenr("facebook.txt");
-        //$readmemh("apple.txt", apple);
-		  //$readmemh("banana.txt", banana);
+        $readmemh("apple.txt", apple);
+		  $readmemh("banana.txt", banana);
 	 end
 
 
 	 
     always @(posedge clk)
     begin
-			//index = ((y-100) * image_width + (x-200)) * 3;
+			index = ((y-100) * image_width + (x-200)) * 3;
 			
         if (y < image_height && x < image_width)
         begin
@@ -38,25 +38,25 @@ module imageloader(clk, x, y, red, green, blue);
 				b <= 'd135;
         end
 		  
-		  /*
-		  else if (y >= 100 && y<image_height+100 && x>199 && x<image_width+200  )
+		  
+		  else if ( y<image_height && x<image_width*2 )
 		  begin 
-				index = ((y-100) * image_width + (x-200)) * 3;
-				/*if(apple[index]=='hff &&apple[index+1]=='hff && apple[index+2]== 'hff)
+				index = ((y) * image_width + (x)) * 3;
+				if(apple[index]=='d255 &&apple[index+1]=='d255 && apple[index+2]== 'd255)
 					begin
 					r <= 'd45;
 					g <= 'd120;
 					b <= 'd135;
 					end
 				else
-					//begin
+					begin
 					r <= apple[index];
 					g <= apple[index+1];
 					b <= apple[index+2];
-					//end
+					end
 				
 				
-		  end*/
+		  end
 		  
         else
         begin
@@ -65,11 +65,8 @@ module imageloader(clk, x, y, red, green, blue);
             b <= 'd135;
         end
 		  if(x==0 && y==0)
-				begin
-				r<=150;
-				g<=150;
-				b<=150;
-				end
+				r<=1;
+				
     end
 
 	assign red = r;
--- Quartus/output_files/FPGAPOSMachine.cdf
+++ Quartus/output_files/FPGAPOSMachine.cdf
@@ -3,6 +3,10 @@ JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("C:/Users/beyza/Documents/GitHub/FPGA-POS-Machine/Quartus/output_files/") File("FPGAPOSMachine.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
